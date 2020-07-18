f3d0de7671d871145a8c32c08bb581131c9d32c2
Zac Smith <zrs3141592@gmail.com>
2020-07-17 20:44:28 -0600

    ex4 complete, 2 tests OK

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -2,7 +2,14 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    cache = {}
+    result = []
+
+    for num in a:
+        cache[num] = 0
+
+        if num is not 0 and -num in cache:
+            result.append(abs(num))
 
     return result
 
