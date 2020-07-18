f155a70baf2bc5ffbc248192557117d2198712d8
Zac Smith <zrs3141592@gmail.com>
2020-07-17 19:43:02 -0600

    ex 1 complete, 4 tests OK

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,15 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+
+    # cache = dict()
+    cache = {}
+
+    for i in range(length):
+        if weights[i] in cache:
+            index_location = cache[weights[i]]
+            return (i, index_location)
+
+        cache[limit - weights[i]] = i
 
     return None
